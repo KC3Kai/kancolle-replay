9bfbdaa7ff86fc57ccc580a0e724a4935a68a4c6
Vultren <vewalden@gmail.com>
2017-03-06 08:35:53 -0500

    Filling in a few fields, working on OTrop phase, added engagement

--- playertext.js
+++ playertext.js
@@ -3,6 +3,23 @@ var TEXTDATA = {
 		"text": "Sortied to <0>-<1> on <2>.",
 		"values": {}
 	},
+	"FLEET_COMPOSITION": {
+		"text": "Sortieing fleet consisting of <0>",
+		"values": {}
+	},
+	"COMBINDED_FLEET_COMPOSITION": {
+		"text": "Sortieing combined fleet consisting of <0> in the main fleet and <1> in the escort fleet.",
+		"values": {}
+	},
+	"SUPPORT_FLEET_COMPOSITION": {
+		"text": "<0> are on standby with <1> Support.",
+		"values": {
+			1: {
+				1: "Node",
+				2: "Boss"
+			}
+		}
+	},
 	"FORMATION_SELECT": {
 		"text": "Select your fleet formation (<0>).",
 		"values": {
@@ -20,7 +37,7 @@ var TEXTDATA = {
 		}
 	},
 	"BATTLE_START": {
-		"text": "You engage the Abyssal Fleet at Node <0>.",
+		"text": "Abyssal Fleet found at Node <0>.",
 		"values": {}
 	},
 	"DETECTION_F": {
@@ -98,46 +115,90 @@ var TEXTDATA = {
 	
 	},
 	"SUPPORT_START": {
-	
+		"text": "The <0> support fleet has arrived with <1>",
+		"values": {
+			0 : {
+				1: "Node",
+				2: "Boss"
+			},
+			1: {
+				 1: "Aerial Support",
+				 2: "Long Range Torpedo Attack", 
+				 3:"Support Shelling"
+			}
+		}
 	},
 	"SUPPORT_DAMAGE": {
 	
 	},
 	"OPTORP_ATTACK": {
-	
+		"text": "<0> launch a pre-emptive torpedo strike.",
+		"values": {
+			
+		}
 	},
 	"OPTORP_DAMAGE": {
-	
+		"text": "<0> launch a pre-emptive torpedo strike.",
+		"values": {
+			
+		}
 	},
 	"ENGAGEMENT": {
-	
+		"text": "Fleet has a <0> against the enemy",
+		"values": {
+			0:{
+				1: "Parallel Engagement",
+				2: "T Advantage",
+				3: "Head-on Engagement",
+				4:"T Disadvantage"
+			}
+		}
 	},
 	"SHELL_START": {
-	
+		"text": "<0> attacks <1>",
+		"values": {
+			
+		}
 	},
 	"SHELL_NORMAL": {
-	
+		"text": "<0> attacks <1>",
+		"values": {
+			
+		}
 	},
 	"SHELL_DOUBLE": {
-	
+		"text": "<0> attacks <1> twice",
+		"values": {
+			
+		}
 	},
 	"SHELL_CUTIN": {
-	
+		"text": "<0> cut-in attacks <1>",
+		"values": {
+			
+		}
 	},
 	"SHELL DAMAGE": {
 	
 	},
 	"TORP_ATTACK": {
-	
+		"text": "<0> luanches torpedos",
+		"values": {
+			
+		}
 	},
 	"TORP_DAMAGE": {
-	
+		"text": "<0>'s torpedoes hit <1> dealing <2> damage",
+		"values": {
+			
+		}
 	},
 	"DAY_END": {
 	
 	},
 	"NIGHT_START": {
-	
+		"text": "Entering Night Battle.",
+		"values": {}
 	},
 }
 
@@ -151,6 +212,16 @@ function getText(name, args) {
 	return text;
 }
 
+function composeFleet(fleet) {
+	var shipList = [];
+		for (var i = 0; i < fleet.length; i++) {
+			var ship = fleet[i];
+			shipList.push(getShipName(ship.mst_id));
+		}
+	return shipList;	
+}
+
+
 function getShipName(mid) {
 	if (!SHIPDATA[mid]) return mid;
 	return SHIPDATA[mid].name + ' (' + mid + ')';
@@ -162,11 +233,21 @@ function getItemName(mid) {
 	return EQDATA[mid].name + ' (' + mid + ')';
 }
 
+function getMapName(map) {
+	if(!MAPDATA[map]) return map;
+	return MAPDATA[map].name;
+}
+
 function getNodeLetter(world,map,num) {
 	var edge;
 	if (!(edge = EDGES['World '+world+'-'+map])) return num;
 	if (!(edge = edge[num])) return num;
 	return edge[1];
+}
+
+
+function getAttackOrder(battle,combined) {
+	
 }
 
 function addText(text) {
@@ -176,13 +257,29 @@ function addText(text) {
 function processText(API) {
 	var world = API.world;
 	var map = API.mapnum;
+	var combined = API.combined
 	addText(getText("SORTIE_START",[world, map, new Date(API.time*1000)]));
+	
+	if(!combined) {
+		addText(getText("FLEET_COMPOSITION",[composeFleet(API['fleet'+API.fleetnum])]));
+	} else {
+		addText(getText("COMBINDED_FLEET_COMPOSITION",[composeFleet(API.fleet1), composeFleet(API.fleet2)]));
+	}
+	
+	if(API.support1 > 0) {
+		addText(getText("SUPPORT_FLEET_COMPOSITION", [composeFleet(API['fleet'+API.support1]), 1]));
+	}
+	
+	if(API.support2 > 0) {
+		addText(getText("SUPPORT_FLEET_COMPOSITION", [composeFleet(API['fleet'+API.support2]), 2]));
+	}
+	
 	addText("");
 
 	for (var b=0; b<API.battles.length; b++) {
 		var battle = API.battles[b];
 		var data = battle.data;
-		
+		var yasen = battle.yasen;
 		var combinedE = (data.api_ship_ke_combined)? 1 : 0;
 		
 		if (data.api_formation) {
@@ -194,6 +291,7 @@ function processText(API) {
 		if (data.api_search) {
 			addText(getText("DETECTION_F", [data.api_search[0]]));
 			addText(getText("DETECTION_E", [data.api_search[1]]));
+			
 		}
 		
 		if (data.api_formation) {
@@ -207,6 +305,62 @@ function processText(API) {
 				enemyList.push(getShipName(data.api_ship_ke[i]));
 			}
 			addText(getText("ENEMY_COMPOSITION",[enemyList]));
+		}
+		
+		//jet LBAS phase
+		if (data.api_air_base_injection) {
+			
+		}
+		
+		//jet phase
+		if (data.api_injection_kouku){}
+		
+		//land bombing
+		if (data.api_air_base_attack) {
+			
+		}
+		
+		//air phase
+		if (data.api_kouku) {}
+		
+		//support phase
+		if (data.api_support_info) {
+			
+		}
+		
+		//opening asw
+		if (data.api_opening_taisen){}
+		
+		//opening torp
+		if (data.api_opening_atack){
+			var fleet = (combined) ? API.fleet2 : API['fleet'+API.fleetnum];
+			var ships = [];
+			
+			for(var i = 0; i < fleet.length; i++) {
+				if(data.api_opening_atack.api_fcl[i+1] == 1) {
+					var ship = fleet[i];
+					ships.push(getShipName(ship.mst_id));
+				}
+				
+			}
+			if(ships.length > 0) {
+				addText(getText("OPTORP_ATTACK", [ships]));	
+			}
+		}
+		
+		
+		//if air node, second air phase
+		if (data.api_kouku2){}
+		
+		if (data.api_formation) {
+			addText(getText("ENGAGEMENT", [data.api_formation[2]]));
+		}
+
+		//shelling 1, 2, 3
+		
+		if(yasen.length > 0) {
+			addText(getText("NIGHT_START",[]));
+			
 		}
 		
 		addText("");
