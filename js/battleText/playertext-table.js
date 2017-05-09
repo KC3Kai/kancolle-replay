ba997fec23e9e59bf2837a1f16920c4d963e8341
fourinone <pz200411@gmail.com>
2017-05-08 20:54:08 -0500

    battle log: show air parity, add text bolding, reformat fleet listing

--- js/battleText/playertext-table.js
+++ js/battleText/playertext-table.js
@@ -1,5 +1,6 @@
-function SHIP(n, hp, max) {
-	this.name = n;
+function SHIP(mid, hp, max) {
+	this.mid = mid;
+	this.name = this.getShipName();
 	this.curHP = hp;
 	this.MaxHP = max;
 }
@@ -12,19 +13,21 @@ SHIP.prototype.isSunk = function() {
 	return this.curHP <= 0;
 };
 
+SHIP.prototype.getShipName = function(includeID) {
+	if (!SHIPDATA[this.mid])
+		return this.mid;
+	let name = SHIPDATA[this.mid].name;
+	if (includeID) name += ' ('+this.mid+')';
+	return name;
+	// return SHIPDATA[mid].nameJP + ' (' + mid + ')';
+};
+
 FLEET = function() {
 	this.mainFleet;
 	this.escortFleet;
 	this.supportNode;
 	this.supportBoss;
 	this.combined = false;
-	
-	getShipName = function(mid) {
-		if (!SHIPDATA[mid])
-			return mid;
-		return SHIPDATA[mid].name;
-		// return SHIPDATA[mid].nameJP + ' (' + mid + ')';
-	};
 };
 
 FLEET.prototype.addFleet = function(fleet, hp) {
@@ -32,15 +35,15 @@ FLEET.prototype.addFleet = function(fleet, hp) {
 	for (var i = 0; i < fleet.length; i++) {
 		if ((fleet[i].mst_id && fleet[i].mst_id == -1) || fleet[i] == -1)
 			continue;
-		this.mainFleet.push(new SHIP(getShipName((fleet[i].mst_id) ? fleet[i].mst_id : fleet[i]), hp[i]));
+		this.mainFleet.push(new SHIP((fleet[i].mst_id) ? fleet[i].mst_id : fleet[i], hp[i]));
 	}
 };
 FLEET.prototype.addCombinedFleet = function(main, escort, hp, ehp, combinedType) {
 	this.mainFleet = [], this.escortFleet = [];;
 	this.combined = combinedType;
 	for (var i = 0; i < main.length; i++) {
-		this.mainFleet.push(new SHIP(getShipName((main[i].mst_id) ? main[i].mst_id : main[i]), hp[i]));
-		this.escortFleet.push(new SHIP(getShipName((escort[i].mst_id) ? escort[i].mst_id : escort[i]), ehp[i]));
+		this.mainFleet.push(new SHIP((main[i].mst_id) ? main[i].mst_id : main[i], hp[i]));
+		this.escortFleet.push(new SHIP((escort[i].mst_id) ? escort[i].mst_id : escort[i], ehp[i]));
 	}
 };
 
@@ -48,12 +51,12 @@ FLEET.prototype.addSupport = function(fleet, boss) {
 	if (boss) {
 		this.supportBoss = [];
 		for (var i = 0; i < fleet.length; i++) {
-			this.supportBoss.push(new SHIP(getShipName((fleet[i].mst_id) ? fleet[i].mst_id : fleet[i])));
+			this.supportBoss.push(new SHIP((fleet[i].mst_id) ? fleet[i].mst_id : fleet[i]));
 		}
 	} else {
 		this.supportNode = [];
 		for (var i = 0; i < fleet.length; i++) {
-			this.supportNode.push(new SHIP(getShipName((fleet[i].mst_id) ? fleet[i].mst_id : fleet[i])));
+			this.supportNode.push(new SHIP((fleet[i].mst_id) ? fleet[i].mst_id : fleet[i]));
 		}
 	}
 };
@@ -78,17 +81,20 @@ FLEET.prototype.isCombined = function(){
 };
 
 
-FLEET.prototype.toString = function() {
+FLEET.prototype.toString = function(delimiter) {
+	if (delimiter === undefined) delimiter = "<br>";
+	
 	var ships = "";
 	if (this.mainFleet.length == 1)
 		return this.mainFleet[0].name;
 
 	for (var i = 0; i < this.mainFleet.length; i++) {
-		if (i == (this.mainFleet.length - 1)) {
-			ships = ships.concat("and ", this.mainFleet[i].name);
-			continue;
-		}
-		ships = ships.concat(this.mainFleet[i].name, ", ");
+		// if (i == (this.mainFleet.length - 1)) {
+			// ships = ships.concat("and ", this.mainFleet[i].name);
+			// continue;
+		// }
+		ships = ships.concat(this.mainFleet[i].name + ' (' + this.mainFleet[i].mid + ')');
+		if (i < this.mainFleet.length - 1) ships = ships.concat(delimiter);
 	}
 	return ships;
 };
