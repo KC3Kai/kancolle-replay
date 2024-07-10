3eef1d819031b920eb2fa2d9a696287b16317862
fourinone41 <pz200411@gmail.com>
2024-07-10 08:41:47 -0400

    sortie sim: Create Shared Link (TinyURL), screenshot button for Statistics (#80), collapsible Mechanics (Show Advanced update visual), kcnav/autobonus sort events desc

--- js/simulator-ui/common.js
+++ js/simulator-ui/common.js
@@ -104,6 +104,25 @@ var COMMON = {
 		return hash;
 	},
 	
+	arrayBufferToBase64: function(buffer) {
+		let binary = '';
+		let bytes = new Uint8Array(buffer);
+		let len = bytes.byteLength;
+		for (let i = 0; i < len; i++) {
+			binary += String.fromCharCode(bytes[i]);
+		}
+		return window.btoa(binary);
+	},
+	base64ToArrayBuffer: function(base64) {
+		let binary_string = window.atob(base64);
+		let len = binary_string.length;
+		let bytes = new Uint8Array(len);
+		for (let i = 0; i < len; i++) {
+			bytes[i] = binary_string.charCodeAt(i);
+		}
+		return bytes;
+	},
+	
 	_init: function() {
 		//https://github.com/tc39/proposal-relative-indexing-method#polyfill
 		function at(n) {
