9dc7f1f4098581f443224da459ad321922914be3
fourinone41 <pz200411@gmail.com>
2024-08-01 05:49:25 -0400

    update bonus: 59-3

--- js/simulator-ui/bonus.js
+++ js/simulator-ui/bonus.js
@@ -17,6 +17,7 @@ COMMON.BONUS_MANAGER = {
 		'58-4': { name: 'Early-Spring 2024 E4' },
 		'59-1': { name: 'Summer 2024 E1' },
 		'59-2': { name: 'Summer 2024 E2' },
+		'59-3': { name: 'Summer 2024 E3' },
 	},
 	_URL_DEWY_INDEX: 'https://api.github.com/repos/sorewachigauyo/kc-event-bonus/git/trees/master?recursive=1',
 	_URL_DEWY_PATH: 'https://raw.githubusercontent.com/sorewachigauyo/kc-event-bonus/master/',
